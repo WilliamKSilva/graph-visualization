936d01ec00088356b26e54400342c8e16d6d3f1d
WilliamKSilva <wilz.kelvin12@gmail.com>
2024-07-28 09:14:21 -0300

    feat: add graph GameObject, shaders (URP and default) and Point prefab

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -14,29 +14,55 @@ public class Graph : MonoBehaviour
     [SerializeField]
     bool parabola = false;
 
+    [SerializeField]
+    bool cubic = false;
+
+    Transform[] points;
+
     void Awake()
     {
         float step = 2f / resolution;
         Vector3 position = Vector3.zero;
         Vector3 scale = Vector3.one * step;
+        points = new Transform[resolution];
         for (int i = 0; i < resolution; i++)
         {
             Transform point = Instantiate(pointPrefab);
+            points[i] = point;
             position.x = (i + 0.5f) * step - 1f;
-            if (linear) {
-                // Linear = Y equals X
-                position.y = position.x;
-            }
 
-            if (parabola) {
-                // Parabola = Y equals X²
-                position.y = position.x * position.x;
-            }
-            
             point.localPosition = position;
             point.localScale = scale;
 
             point.SetParent(transform, false);
+        }
+    }
+
+    void Update()
+    {
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++)
+        {
+            Vector3 position = points[i].localPosition;
+            if (linear)
+            {
+                // Linear = Y equals X
+                position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            }
+
+            if (parabola)
+            {
+                // Parabola = Y equals X²
+                position.y = Mathf.Sin(Mathf.PI * (position.x * position.x + time));
+            }
+
+            if (cubic)
+            {
+                // Cubic = Y equals X³
+                position.y = Mathf.Sin(Mathf.PI * (position.x * position.x * position.x + time));
+            }
+
+            points[i].localPosition = position;
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,12 @@
+# Demo
+
+
+# Info
+This is a function Graph visualization, we currently have three available, you can change between then on the Graph GameObject settings of the inspector:
+- Linear
+- Parabola
+- Cubic
+
+This project have two versions of the same Shader that is used to make the Graph colorful:
+  - The first is the PointSurface.shader, that makes use of the Default Unity render pipeline, built directly with shader language code
+  - The second is the PointURP.shadergraph, that makes use of the Universal Render Pipeline, built using shader graph (a graphical solution to build shaders on Unity editor).
